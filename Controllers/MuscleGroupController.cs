5d593409e290242e2119baed77821fca4545562d
Germán <german.marin_acosta@contractors.roche.com>
2023-11-28 15:45:19 +0100

    Cambiamos el origen de datos, de la tabla de muestra de ciudades a la propia de grupos musculares

---
+++ Controllers/MuscleGroupController.cs
@@ -0,0 +1,66 @@
+﻿using ApiMySQL.Repositories;
+using ApiMySQL.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiMySQL.Controllers
+{
+        [Route("api/[controller]")]
+        [ApiController]
+        public class MuscleGroupController : ControllerBase
+        {
+            private readonly IMuscleGroupRepository _muscleGroupRepository;
+
+            public MuscleGroupController(IMuscleGroupRepository muscleGroupRepository)
+            {
+                _muscleGroupRepository = muscleGroupRepository;
+            }
+            [HttpGet]
+            public async Task<IActionResult> GetAllMuscleGroup()
+            {
+                return Ok(await _muscleGroupRepository.GetAllMuscleGroup());
+            }
+
+            [HttpGet("{id}")]
+            public async Task<IActionResult> GetMuscleGroup(int id)
+            {
+                return Ok(await _muscleGroupRepository.GetMuscleGroup(id));
+            }
+
+            [HttpPost]
+            public async Task<IActionResult> InsertMuscleGroup([FromBody] MuscleGroup muscleGroup)
+            {
+                if (muscleGroup == null)
+                    return BadRequest();
+
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                var created = await _muscleGroupRepository.InsertMuscleGroup(muscleGroup);
+                return Created("created", created);
+
+            }
+
+            [HttpPut]
+            public async Task<IActionResult> UpdateMuscleGroup([FromBody] MuscleGroup muscleGroup)
+            {
+                if (muscleGroup == null)
+                    return BadRequest();
+
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                await _muscleGroupRepository.UpdateMuscleGroup(muscleGroup);
+                return NoContent();
+
+            }
+            [HttpDelete]
+            public async Task<IActionResult> DeleteMuscleGroup(int id)
+            {
+                await _muscleGroupRepository.DeleteMuscleGroup(id);
+                return NoContent();
+            }
+
+
+
+        }
+    }
---
+++ Repositories/MuscleGroupRepository.cs
@@ -0,0 +1,85 @@
+﻿using ApiMySQL.Data;
+using ApiMySQL.Model;
+using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace ApiMySQL.Repositories
+{
+    public class MuscleGroupRepository : IMuscleGroupRepository
+
+        {
+            private readonly MySQLConfiguration _connectionString;
+
+            public MuscleGroupRepository(MySQLConfiguration connectionString)
+            {
+                _connectionString = connectionString;
+            }
+
+            protected MySqlConnection DbConnection()
+            {
+                return new MySqlConnection(_connectionString.ConnectionString);
+            }
+
+            public async Task<bool> DeleteMuscleGroup(int id)
+            {
+                var db = DbConnection();
+
+                var sql = @"DELETE FROM grupo_muscular
+                        WHERE ID_grupo = @Id ";
+                var result = await db.ExecuteAsync(sql, new { Id = id });
+
+                return result > 0;
+            }
+
+            public async Task<IEnumerable<MuscleGroup>> GetAllMuscleGroup()
+            {
+              var db = DbConnection();
+            
+            //Averiguar otra manera de hacer esto sin tener que poner alias a las columnas
+                var sql = @"SELECT ID_grupo as ID, descripcion as Description, Imagen_frente as ImageFront, Imagen_trasera as ImageRear
+                        FROM grupo_muscular";
+
+                return await db.QueryAsync<MuscleGroup>(sql, new { });
+            }
+
+            public async Task<MuscleGroup> GetMuscleGroup(int id)
+            {
+                var db = DbConnection();
+            //Averiguar otra manera de hacer esto sin tener que poner alias a las columnas
+            var sql = @"SELECT ID_grupo as ID, descripcion as Description, Imagen_frente as ImageFront, Imagen_trasera as ImageRear
+                        FROM grupo_muscular
+                        WHERE ID_grupo = @Id ";
+
+                return await db.QueryFirstOrDefaultAsync<MuscleGroup>(sql, new { Id = id });
+            }
+
+            public async Task<bool> InsertMuscleGroup(MuscleGroup muscleGroup)
+            {
+                var db = DbConnection();
+
+                var sql = @"INSERT INTO grupo_muscular(descripcion, Imagen_frente, Imagen_trasera, f_ult_act)
+                       VALUES(@Description, @ImageFront, @ImageRear, CURRENT_TIMESTAMP)";
+
+                var result = await db.ExecuteAsync(sql, new { muscleGroup.Description, muscleGroup.ImageFront, muscleGroup.ImageRear });
+
+                return result > 0;
+
+            }
+
+            public async Task<bool> UpdateMuscleGroup(MuscleGroup muscleGroup)
+            {
+                var db = DbConnection();
+
+                var sql = @"UPDATE grupo_muscular
+                            SET  descripcion = @Description, 
+                                 Imagen_frente = @ImageFront, 
+                                 Imagen_trasera = @ImageRear, 
+                                 f_ult_act = CURRENT_TIMESTAMP
+                            WHERE ID_grupo = @Id ";
+
+                var result = await db.ExecuteAsync(sql, new { muscleGroup.Description, muscleGroup.ImageFront, muscleGroup.ImageRear, Id = muscleGroup.ID });
+
+                return result > 0;
+            }
+        }
+    }
